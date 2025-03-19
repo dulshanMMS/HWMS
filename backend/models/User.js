f2bc1dcb094be8d2f639b4bcefe78f3750ba600d
MNKolombage <mkolombage@gmail.com>
2025-03-20 00:02:06 +0530

    fix: fixed mistakes in rolebase management in signin

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,8 @@ const UserSchema = new mongoose.Schema({
     lastName: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    role: { type: String, enum: ["admin", "user"], default: "user" },
 });
 
 export default mongoose.model("User", UserSchema);
