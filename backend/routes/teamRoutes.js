c76af5a2db1878d642c8f0ef296f959c0a05d648
MNKolombage <mkolombage@gmail.com>
2025-06-05 15:03:21 +0530

    feat: fetching team members of the team

--- backend/routes/teamRoutes.js
+++ backend/routes/teamRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { getAllTeams, addTeam } from '../controllers/teamController.js';
+import { getAllTeams, addTeam, getTeamMembers } from '../controllers/teamController.js';
 
 const router = express.Router();
 
 // GET /api/teams - Fetch all team color details from here
 router.get('/', getAllTeams);
 router.post('/', addTeam);
+
+// GET /api/teams/:teamId/members - Fetch users in a team
+router.get('/:teamId/members', getTeamMembers);
 
 export default router;
