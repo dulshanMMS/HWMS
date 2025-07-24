29e1326e4109164a302985630b044e866021df8a
tobey028 <168511064+tobey028@users.noreply.github.com>
2025-07-24 20:29:22 +0530

    Merge with dev2 /port 5000

--- backend/server.js
+++ backend/server.js
@@ -128,5 +128,5 @@ io.on("connection", (socket) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 6001;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
