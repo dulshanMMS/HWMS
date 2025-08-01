703e2d68a36c9b654c2885d7d2d9286ba81746cd
tobey028 <168511064+tobey028@users.noreply.github.com>
2025-08-01 10:07:40 +0530

    with 5000 port

--- backend/server.js
+++ backend/server.js
@@ -141,5 +141,5 @@ io.on("connection", (socket) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 6001;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
